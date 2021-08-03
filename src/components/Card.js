b1e780450428be6b34963c57393bee82f7b4bb0b
Musty <grigoras.tiberiu@gmail.com>
2021-08-04 00:05:17 +0300

    fixed img loading

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,11 @@
 import '../styles/Card.css'
 
 const Card = (props) => {
-    const imgSource = `/assets/${props.imgSource}.png`
     const cardClicked = props.cardClicked
 
     return(
         <div className="main-card" id={`${props.title}` } onClick={() => cardClicked(props.title)}>
-            <img alt="" src={imgSource} />
+            <img alt="" src={require(`../assets/${props.imgSource}.png`).default} />
             <h3 id="title">{props.title}</h3>
         </div>
     )
